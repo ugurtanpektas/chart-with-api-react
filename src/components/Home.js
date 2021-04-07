e65fa75dd632826fbcdd859e19913aadbbfae9a2
Ugurtan <ugurtanpektas@scorpapp.com>
2021-04-07 16:52:01 +0300

    react-google-chart package added and sample chart added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,22 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {userAction} from './../actions/userAction';
 import Navbar from './../components/Navbar';
-import Sidebar from './../components/Sidebar'
+import Sidebar from './../components/Sidebar';
+import { Chart } from "react-google-charts";
+
+const options = {
+    hAxis: { title: "Date", viewWindow: { min: 0, max: 15 } },
+    vAxis: { title: "Degree (Celcius)", viewWindow: { min: 0, max: 35 } }
+  };
+  const data = [
+    ["x", "Degree"],
+    [8, 12],
+    [4, 5.5],
+    [11, 14],
+    [4, 5],
+    [3, 3.5],
+    [6.5, 7]
+  ];
 
 class Home extends React.Component{
 
@@ -18,6 +33,14 @@ class Home extends React.Component{
                 <Sidebar />
                 <div class="main">
                 <h1>Daily</h1>
+                <Chart
+                    chartType="LineChart"
+                    data={data}
+                    options={options}
+                    width="100%"
+                    height="400px"
+                    legendToggle
+                    />
                 </div>
             </div>
         )
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
     email:null,
-    isLoggedIn : true,
+    isLoggedIn : false,
     loading:true
 };
 
